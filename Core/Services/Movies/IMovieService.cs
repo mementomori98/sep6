b267f429661be9c62a6d639564e485dcf2d15c3b
mementomori98 <sebestyen.mate98@gmail.com>
2021-05-12 12:05:07 +0200

    Revert "Revert "Cleanup and receive""
    This reverts commit af89ba66a286f4d09446b857f3c4cf3e168a237d.

---
+++ Core/Services/Movies/IMovieService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Services.Movies.Models;
+
+namespace Core.Services.Movies
+{
+    public interface IMovieService
+    {
+        Task<IEnumerable<MovieListItem>> SearchList(string text);
+    }
+}
--- Notflix/Startup.cs
+++ Notflix/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Services.Movies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -31,6 +32,7 @@ namespace Notflix
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
             services.AddMudServices();
+            services.AddTransient<IMovieService, OmdbMovieService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
