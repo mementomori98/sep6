4f24a6e1c8f5235f2d3069602ed08316ebb08f60
Zoltan Vegh <zotyovegh@gmail.com>
2021-05-12 15:40:16 +0200

    Favourites menu, item and dummy test

---
+++ Core/Services/Movies/Models/FavouriteMovieItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services.Movies.Models
+{
+    public class FavouriteMovieItem
+    {
+        public string Title { get; set; }
+        public string ImdbId { get; set; }
+        public string Poster { get; set; }
+        public string Plot { get; set; }
+    }
+}
