709690e92d5c019010b1e54bbe47a8350e460ec3
Zoltan Vegh <zotyovegh@gmail.com>
2021-05-13 14:25:43 +0200

    Movies (Main) page's first version with iconCards

---
+++ Core/Services/Movies/Models/MovieIconCardItem.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Core.Services.Movies.Models
+{
+    public class MovieIconCardItem
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Director { get; set; }
+        public string Length { get; set; }
+        public string Poster { get; set; }
+        public string Rating { get; set; }
+        public string ImdbId { get; set; }
+        public string Plot { get; set; }
+    }
+}
